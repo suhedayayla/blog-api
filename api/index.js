8d83ff27f39e248a4149fb175200bcb51196c73a
Suheda Yayla <suhedayayla1@gmail.com>
2023-11-14 14:09:48 +0300

    blog all api

---
+++ api/index.js
@@ -0,0 +1,43 @@
+const express=require("express");
+const app=express();
+const dotenv=require("dotenv");
+const mongoose=require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+
+dotenv.config();
+app.use(express.json()); //postmande test ederken json tipiyle çalışması için bunu ekledik
+
+mongoose.connect(process.env.MONGO_URL,)
+        .then(console.log("Connected to monggodb"))
+        .catch(err=>console.log(err));
+
+        //images için;
+        const storage = multer.diskStorage ({
+            destination:(req,file,cb)=> {   //cb=callback funtion,takecare any errors
+                cb(null,"images"); //images is our picturees saved file
+            },
+            filename:(req,file,cb) => {
+                cb(null,req.body.name);
+            },
+        });
+        const upload = multer({storage:storage});
+        //we use post because we create something
+        //we use upload.single because we upload 1 picture.
+        app.post("/api/upload",upload.single("file"),(req,res)=>{
+            res.status(200).json("Dosya başarıyla yüklendi.")
+
+        })
+
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
+app.use("/api/posts",postRoute);
+app.use("/api/categories",categoryRoute);
+
+
+app.listen("5000",()=> {
+    console.log("Backend is running.")
+});
